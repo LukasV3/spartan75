5fcf0eb64d9e3200ed7fb14b3bb2e0620281f880
Lukas Volk <volk.lukas237@yahoo.co.uk>
2025-01-13 13:44:34 +0000

    Moved dashboard page to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,62 @@
-export default function Home() {
+"use client";
+
+import { useState } from "react";
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import DailyTasksChecklist from "@/components/DailyTasksChecklist";
+import ProgressOverview from "@/components/ProgressOverview";
+
+export default function Page() {
+  const [currentStreak, setCurrentStreak] = useState(0);
+  const currentDayIndex = 1;
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Home
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+
+            <Separator orientation="vertical" className="mr-2 h-4" />
+
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Dashboard</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0 container mx-auto">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-[1fr,_min-content]">
+            <DailyTasksChecklist
+              incrementStreak={() => setCurrentStreak((a) => a + 1)}
+              decrementStreak={() => setCurrentStreak((a) => a - 1)}
+              currentDayIndex={currentDayIndex}
+            />
+
+            <ProgressOverview
+              currentStreak={currentStreak}
+              currentDayIndex={currentDayIndex}
+            />
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
