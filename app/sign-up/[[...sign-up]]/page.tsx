99ea5ebf8edd66e1cff0a032f1cad276f34e2d6c
Lukas Volk <volk.lukas237@yahoo.co.uk>
2025-01-16 16:49:41 +0000

    Removed dialog logic from sign-in and sign-out pages

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,27 +1,9 @@
-"use client";
-
-import React from "react";
-import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
-import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import { SignUp } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
 
 export default function Page() {
-  const router = useRouter();
-
   return (
-    <Dialog defaultOpen={true} onOpenChange={() => router.push("/")}>
-      <DialogContent className="w-min p-0 !rounded-[0.75rem]">
-        {/* to ensure accessibility for screen reader users a dialog title is needed. */}
-        <VisuallyHidden asChild>
-          <DialogTitle>Sign Up</DialogTitle>
-        </VisuallyHidden>
-
-        <SignUp />
-      </DialogContent>
-
-      {/* TODO: display app in background */}
-      {/* <App /> */}
-    </Dialog>
+    <div className="grid place-content-center h-screen pb-20">
+      <SignUp />
+    </div>
   );
 }
