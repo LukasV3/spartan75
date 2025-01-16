0968744274b21e9d43b095a75851a250c5bd7d3f
Lukas Volk <volk.lukas237@yahoo.co.uk>
2025-01-16 16:42:15 +0000

    Refactor dashboard components and folder structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
-import { AppSidebar } from "@/components/sidebar/sidebar";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbList,
-  BreadcrumbPage,
-} from "@/components/ui/breadcrumb";
-import { Separator } from "@/components/ui/separator";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/sidebar/app-sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import Dashboard from "@/components/dashboard/dashboard";
 import { auth } from "@clerk/nextjs/server";
 
@@ -23,25 +12,7 @@ export default async function Page() {
     <SidebarProvider>
       <AppSidebar />
 
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-
-            <Separator orientation="vertical" className="mr-2 h-4" />
-
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Dashboard</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
-
-        <Dashboard />
-      </SidebarInset>
+      <Dashboard />
     </SidebarProvider>
   );
 }
--- components/dashboard/dashboard.tsx
+++ components/dashboard/dashboard.tsx
@@ -1,6 +1,8 @@
+import Header from "@/components/dashboard/header";
 import Tasks from "@/components/dashboard/tasks-checklist/tasks";
 import ProgressOverview from "@/components/dashboard/progress-overview/progress-overview";
 import { fetchUserChallengeStartDate } from "@/lib/data";
+import { SidebarInset } from "@/components/ui/sidebar";
 
 export default async function Dashboard() {
   const challengeStartDate = await fetchUserChallengeStartDate();
@@ -20,12 +22,16 @@ export default async function Dashboard() {
   };
 
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4 pt-0 container mx-auto">
-      <div className="grid auto-rows-min gap-4 md:grid-cols-[1fr,_min-content]">
-        <Tasks currentDayIndex={currentDayIndex()} />
+    <SidebarInset>
+      <Header />
 
-        <ProgressOverview currentDayIndex={currentDayIndex()} />
+      <div className="flex flex-1 flex-col gap-4 p-4 pt-0 container mx-auto">
+        <div className="grid auto-rows-min gap-4 md:grid-cols-[1fr,_min-content]">
+          <Tasks currentDayIndex={currentDayIndex()} />
+
+          <ProgressOverview currentDayIndex={currentDayIndex()} />
+        </div>
       </div>
-    </div>
+    </SidebarInset>
   );
 }
