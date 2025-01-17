c1be92e7c93157b754c7eab92e1e3f77a5bb6a61
Lukas Volk <volk.lukas237@yahoo.co.uk>
2025-01-17 17:36:58 +0000

    Implemented dark mode toggle

--- components/dashboard/header.tsx
+++ components/dashboard/header.tsx
@@ -6,11 +6,15 @@ import {
 } from "@/components/ui/breadcrumb";
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { ModeToggle } from "@/components/dashboard/mode-toggle";
+import { Github } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-      <div className="flex items-center gap-2 px-4">
+      <div className="flex flex-1 items-center gap-2 px-4">
         <SidebarTrigger className="-ml-1" />
 
         <Separator orientation="vertical" className="mr-2 h-4" />
@@ -22,6 +26,16 @@ const Header = () => {
             </BreadcrumbItem>
           </BreadcrumbList>
         </Breadcrumb>
+
+        <div className="ml-auto pr-3 flex items-center">
+          <Button variant="ghost" size="icon" asChild>
+            <Link href="https://github.com/LukasV3" target="_blank">
+              <Github />
+            </Link>
+          </Button>
+
+          <ModeToggle />
+        </div>
       </div>
     </header>
   );
